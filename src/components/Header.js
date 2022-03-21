56b0022a25bae62eb26a8601a309026dde4221e4
Janne-R <74651035+Janne-R@users.noreply.github.com>
2022-03-21 09:55:11 +0100

    setup

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+//import styled from "styled-components";
+import BackgroundImage from "./BackgroundImage";
+
+const Header = () => (
+  <BackgroundImage img={"image/header.jpg"} height={"420px"} />
+
+
+
+);
+
+export default Header;

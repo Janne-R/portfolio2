aa80d32d885838e5ea2d34ebf49298f70a495c2d
Janne-R <74651035+Janne-R@users.noreply.github.com>
2022-03-23 20:05:42 +0100

    Home page responsive done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import { NavLink } from "react-router-dom";
-import { HandWrittenH3 } from "./Handwritten";
+import { HandWrittenH3, HandWrittenP } from "./Handwritten";
 
 const Ul = styled.ul`
 text-align: center;
@@ -15,6 +15,25 @@ a{
     text-decoration: none;
     color: ${({ theme }) => theme.colors.dark};
   }
+
+  @media ${({ theme }) => theme.devices.tabletS} { 
+    display: flex;
+    justify-content: space-around;
+  }
+`;
+
+const Title = styled(HandWrittenH3)`
+font-weight: bolder;
+`;
+
+const Name = styled(HandWrittenP)`
+font-size: 28px;
+font-weight: bolder;
+margin-bottom: 0;
+`;
+
+const P = styled.p`
+margin-top: 0;
 `;
 
 const CopyRight = styled.div`
@@ -24,38 +43,55 @@ text-align: center;
 padding: 16px;
 `;
 
+const TabletDiv = styled.div`
+  display: none;
+  
+  @media ${({ theme }) => theme.devices.tabletS} { 
+    display: block;
+  }
+
+`;
+
 const Footer = () => {
   return (
     <>
       <Ul>
-        <HandWrittenH3>Navigation</HandWrittenH3>
-        <li>
-          <NavLink exact to="/" >
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/portfolio" >
-            Portfolio
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/about" >
-            About
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/contact" >
-            Contact
-          </NavLink>
-        </li>
-        <HandWrittenH3>Links</HandWrittenH3>
-        <li>
-          <a href="http://www.linkedin.com/in/janneringdal">LinkedIn</a>
-        </li>
-        <li>
-          <a href="https://github.com/Janne-R">GitHub</a>
-        </li>
+        <div>
+          <Title>Navigation</Title>
+          <li>
+            <NavLink exact to="/" >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/portfolio" >
+              Portfolio
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/about" >
+              About
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact" >
+              Contact
+            </NavLink>
+          </li>
+        </div>
+        <TabletDiv>
+          <Name>Janne Ringdal</Name>
+          <P>Front end developer</P>
+        </TabletDiv>
+        <div>
+          <Title>Links</Title>
+          <li>
+            <a href="http://www.linkedin.com/in/janneringdal">LinkedIn</a>
+          </li>
+          <li>
+            <a href="https://github.com/Janne-R">GitHub</a>
+          </li>
+        </div>
       </Ul>
       <CopyRight>Copyright ©Janne Ringdal</CopyRight>
     </>

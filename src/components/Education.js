7fe77a93fb4edb1cbb13ac73121dd385cc639cc7
Janne-R <74651035+Janne-R@users.noreply.github.com>
2022-03-24 11:17:16 +0100

    education responsvive

--- src/components/Education.js
+++ src/components/Education.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import { HandWrittenH2 } from "./Handwritten";
 import BlockContainer from "./BlockContainer";
 import Numbers from "./Numbers";
+import ImageWithShadow from "../components/BackgroundImageWithShadow";
 
 const Container = styled.div`
   background-color: ${({ theme }) => theme.colors.secondaryBackgroundColor};
@@ -15,34 +16,65 @@ const H2 = styled(HandWrittenH2)`
 text-align: start;
 `;
 
+const Grid = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 6fr;
+    margin-bottom: 25px;
+
+`;
+
+const P = styled.p`
+    margin-top: 0;
+`;
+
+
+const Image = styled(ImageWithShadow)`
+  height: 360px;
+
+  @media ${({ theme }) => theme.devices.tabletS} { 
+  width: 80%;
+  height: 460px;
+  margin-top: -30px;
+}
+`;
+
+const Flex = styled(BlockContainer)`
+
+@media ${({ theme }) => theme.devices.tabletS} { 
+  display: flex;
+}
+`;
+
 const Education = () => {
   return (
     <Container>
-      <BlockContainer>
-        <H2>Education</H2>
+      <Flex>
         <div>
-          <Numbers>01</Numbers>
-          <p>Lorem ipsum, Lorem ipsum, Lorem ipsum,
-            Lorem ipsum Lorem ipsum Lorem ipsum,
-            Lorem ipsum Lorem ipsum Lorem ipsum
-          </p>
+          <H2>Education</H2>
+          <Grid>
+            <Numbers>01</Numbers>
+            <P>Lorem ipsum, Lorem ipsum, Lorem ipsum,
+              Lorem ipsum Lorem ipsum Lorem ipsum,
+              Lorem ipsum Lorem ipsum Lorem ipsum
+            </P>
+          </Grid>
+          <Grid>
+            <Numbers>02</Numbers>
+            <P>Lorem ipsum, Lorem ipsum, Lorem ipsum,
+              Lorem ipsum Lorem ipsum Lorem ipsum,
+              Lorem ipsum Lorem ipsum Lorem ipsum
+            </P>
+          </Grid>
+          <Grid>
+            <Numbers>03</Numbers>
+            <P>Lorem ipsum, Lorem ipsum, Lorem ipsum,
+              Lorem ipsum Lorem ipsum Lorem ipsum,
+              Lorem ipsum Lorem ipsum Lorem ipsum
+            </P>
+          </Grid>
         </div>
-        <div>
-          <Numbers>02</Numbers>
-          <p>Lorem ipsum, Lorem ipsum, Lorem ipsum,
-            Lorem ipsum Lorem ipsum Lorem ipsum,
-            Lorem ipsum Lorem ipsum Lorem ipsum
-          </p>
-        </div>
-        <div>
-          <Numbers>03</Numbers>
-          <p>Lorem ipsum, Lorem ipsum, Lorem ipsum,
-            Lorem ipsum Lorem ipsum Lorem ipsum,
-            Lorem ipsum Lorem ipsum Lorem ipsum
-          </p>
-        </div>
-
-      </BlockContainer>
+        <Image img="images/design.jpg" height="260px" />
+      </Flex>
     </Container>
   );
 };

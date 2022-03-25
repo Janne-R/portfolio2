36f13d100950a08bd91db804c10ca952eb0a6adf
Janne-R <74651035+Janne-R@users.noreply.github.com>
2022-03-25 10:23:17 +0100

    contact form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,24 +1,67 @@
+import styled from "styled-components";
+import BlockContainer from "./BlockContainer";
+import Button from "./Button";
+import { HandWrittenH2 } from "../components/Handwritten";
+
+const Flex = styled.div`
+display: flex;
+  flex-direction: column;
+  @media ${({ theme }) => theme.devices.tabletS} { 
+width: 80%
+  }
+
+  @media ${({ theme }) => theme.devices.laptopS} { 
+width: 60%
+  }
+`;
+
+const H2 = styled(HandWrittenH2)`
+text-align: start;
+`;
+
+const Label = styled.label`
+margin-bottom: 10px;
+`;
+
+const Input = styled.input`
+border-radius: 10px;
+border: none;
+box-shadow: 0 3px 6px rgba(183, 123, 88);
+padding: 10px;
+margin-bottom: 20px;
+`;
+
+const Textarea = styled.textarea`
+border-radius: 10px;
+border: none;
+box-shadow: 0 3px 6px rgba(183, 123, 88);
+padding: 10px;
+margin-bottom: 30px;
+`;
 
 
 const ContactForm = () => {
   return (
-    <form action="">
-      <div class="form-wrapper">
-        <label for="name">Name</label>
-        <input type="text" id="name" name="Name" placeholder="Full name" />
+    <BlockContainer>
+      <H2>Contact form</H2>
+      <form action="">
+        <Flex>
+          <Label for="name">Name</Label>
+          <Input type="text" id="name" name="Name" placeholder="Full name" />
 
-        <label for="email">Email</label>
-        <input type="email" id="email" name="email" placeholder="example@mail.com" />
+          <Label for="email">Email</Label>
+          <Input type="email" id="email" name="email" placeholder="example@mail.com" />
 
-        <label for="phone">Phone</label>
-        <input type="tel" id="phone" name="phone" placeholder="xxx xx xxx" />
+          <Label for="phone">Phone</Label>
+          <Input type="tel" id="phone" name="phone" placeholder="xxx xx xxx" />
 
-        <label for="message">Message </label>
-        <textarea name="message" id="message" cols="30" rows="10" placeholder="Message"></textarea>
+          <Label for="message">Message </Label>
+          <Textarea name="message" id="message" cols="30" rows="10" placeholder="Message"></Textarea>
 
-      </div>
-      <button>Submit</button>
-    </form>
+        </Flex>
+        <Button>Submit</Button>
+      </form>
+    </BlockContainer>
 
   );
 };

69704d313e58c93ce91daf51dcba67157798cab1
Janne-R <74651035+Janne-R@users.noreply.github.com>
2022-03-25 13:36:14 +0100

    design done

--- src/components/Header.js
+++ src/components/Header.js
@@ -27,7 +27,7 @@ const HeroImg = styled(BackgroundImage)`
     `;
 
 const Header = () => (
-  <HeroImg img={"images/header.jpg"} height={"260px"} />
+  <HeroImg img={"images/headerNew.png"} height={"260px"} />
 );
 
 export default Header;
